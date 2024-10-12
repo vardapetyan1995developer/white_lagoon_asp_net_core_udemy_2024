3916abb42ae507b2e7c2cd4992ffdb960b4064e4
Narek <vardapetyannarek0@gmail.com>
2024-10-13 01:30:55 +0400

    Register application db context to program class file

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -11,7 +11,7 @@ namespace WhiteLagoon.Infrastructure.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-                        
+            
         }
     }
 }
