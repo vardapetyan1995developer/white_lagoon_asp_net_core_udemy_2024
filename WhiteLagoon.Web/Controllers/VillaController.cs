435c595db2cbc8ba4754a4208c5f5b0f8721e3b9
Narek <vardapetyannarek0@gmail.com>
2024-10-14 17:09:25 +0400

    Update action for villa

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -56,5 +56,20 @@ namespace WhiteLagoon.Web.Controllers
 
             return View(obj);
         }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _db.Villas.Update(obj);
+
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", "Villa");
+            }
+
+            return View();
+        }
     }
 }
